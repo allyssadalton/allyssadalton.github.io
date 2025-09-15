16087a416b63114e6b4968f14862b07232c851de
Allyssa Dalton <allyssamarie06@icloud.com>
2025-09-15 02:09:22 +0000

    done for now

--- javascript/CollapsibleTextProject.js
+++ javascript/CollapsibleTextProject.js
@@ -1,15 +1,14 @@
-// CollapsibleTextProject.js
-document.querySelectorAll('.toggleBtn').forEach(button => {
+ document.querySelectorAll('.toggleBtn').forEach(button => {
   button.addEventListener('click', () => {
     const expandable = button.closest('.expandable');
     const extraText = expandable.querySelector('p');
 
     if (!extraText) return;
 
-    extraText.classList.toggle('extraTextHidden');
+    extraText.classList.toggle('show');
 
-    button.textContent = extraText.classList.contains('extraTextHidden')
-      ? 'Show More'
-      : 'Show Less';
+    button.textContent = extraText.classList.contains('show')
+      ? 'Show Less'
+      : 'Show More';
   });
 });
